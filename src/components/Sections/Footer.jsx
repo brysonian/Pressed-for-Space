213a9343a6039ab09a30c6c5d591409039f61265
Darin Buzon <dabuzon@Darins-MacBook-Pro.local>
2019-06-02 14:36:42 -0700

    Another progress check

--- src/components/Sections/Footer.jsx
+++ src/components/Sections/Footer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import Marquee from "react-marquee";
 
 import { Container } from "../../Styles";
@@ -7,10 +8,24 @@ export class Footer extends Component {
   render() {
     return (
       <Container>
-        <Marquee text="Be There or Be Square" loop="true" />
+      <Line />
+      <OtherLine />
+      <Line />
+        {/* <Marquee text="Be There or Be Square" /> */}
       </Container>
     );
   }
 }
+
+const Line = styled.hr`
+  border: none;
+  border-top: 7px solid black;
+  padding: 10px 0;
+`
+
+const OtherLine = styled.hr`
+  border: none;
+  border-top: 100px solid black;
+`
 
 export default Footer;
