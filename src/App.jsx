c811d6cc2572d58211b9424943f5ab93df7f736f
Chandler McWilliams <chandler@brysonian.com>
2019-06-04 10:56:55 -0700

    Updates basename in router adds homepage option to package

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Style } from "./Styles";
@@ -7,14 +7,14 @@ import Home from "./pages/Home";
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <Fragment>
         <Style />
-        <Router>
+        <Router basename="/exhibitions/senior/pressed4space/">
           <Switch>
             <Route path="/" component={Home} exact />
           </Switch>
         </Router>
-      </React.Fragment>
+      </Fragment>
     );
   }
 }
